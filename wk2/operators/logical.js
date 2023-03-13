9436798c3397cb880c8e51c6bea4b60f50680d54
Kirill <heimdall.rofl@gmail.com>
2023-03-13 23:54:39 +0300

    added logical

---
+++ wk2/operators/logical.js
@@ -0,0 +1,13 @@
+// Logical operator
+// 1. &&
+// 2. ||
+// 3. !
+
+// Twi main purposes:
+// 1. In conditional statements (e.g. if...else)
+// 2. To have a default value
+
+//Use case1: AND (&&) check if given val is a text
+
+let val = "Monday"
+console.log(typeof val);
