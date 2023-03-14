11f7c43eca7252f50d9c6b3ae8e649b8aa09e121
Kirill <heimdall.rofl@gmail.com>
2023-03-14 23:22:57 +0300

    added logicalOr

---
+++ wk2/operators/logicalOr.js
@@ -0,0 +1,11 @@
+ // Logical operator OR (||)
+
+ // Check if given day is weekend or not
+
+ let day = "Sunday"
+
+ if (day === "Saturday" || day === "Sunday") {
+    console.log(`Given day: ${day} is a weekend`);
+ } else {
+    console.log(`Given day: ${day} is NOT a weekend`);
+ }
