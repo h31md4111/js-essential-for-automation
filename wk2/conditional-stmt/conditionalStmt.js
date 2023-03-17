189a5fbe2da78cfb28eea9a48ac1f4745650fd80
Kirill <heimdall.rofl@gmail.com>
2023-03-17 23:25:36 +0300

    added ifnot case

--- wk2/conditional-stmt/conditionalStmt.js
+++ wk2/conditional-stmt/conditionalStmt.js
@@ -3,3 +3,7 @@ let popupExist = true
 if (popupExist === true) {
     console.log(`>>> Perform click action...`);
 }
+
+// use case 2: just if (not) - checking for falsy value
+let data = "PASS"
+if(!data) throw Error(">>> No valid data provided")
