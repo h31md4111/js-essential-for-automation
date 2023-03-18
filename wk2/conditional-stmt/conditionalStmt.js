e8790cfd0f66d177b8c965ca9d1c0c2a76a4bee0
Kirill <heimdall.rofl@gmail.com>
2023-03-19 01:14:27 +0300

    nested added

--- wk2/conditional-stmt/conditionalStmt.js
+++ wk2/conditional-stmt/conditionalStmt.js
@@ -7,3 +7,25 @@ if (popupExist === true) {
 // use case 2: just if (not) - checking for falsy value
 let data = "PASS"
 if(!data) throw Error(">>> No valid data provided")
+
+// use case 3: if..else: If file exist read the file, else create the file
+let fileExist = "Y"
+
+if (fileExist === "Y") {
+console.log(`>>> Reading the file`);
+} else {
+console.log(`>>> Creating the file`);
+}
+
+// use case 4: if else if ... Nested conditions... Act based on list of conditions
+let err = "AssertionError"
+
+if (err === "Error") {
+    console.log(`>>> Retry...`);
+} else if (err === "AssertionError") {
+    console.log(`>> Fail the test`);
+} else if (err === "DataValidation") {
+    console.log(`>> Read the test data file...`);
+} else {
+    console.log(`>> Print the error message`);
+}
